224ab1bf901d601118ac33fe1b37527d313e0d71
ShaunVuHSGS <32510034+ShaunVuHSGS@users.noreply.github.com>
2018-10-03 17:01:56 +0700

    Third commit

--- DictCmdVer1/Main.java
+++ DictCmdVer1/Main.java
@@ -1,33 +1,9 @@
 package Dict;
+public class Main {
 
-public class DictionaryCommandline {
-    private  int th;
-    public  void   dictionaryBasic()
-    {
-        DictionaryManagement dm= new DictionaryManagement();
-        dm.insertFromCommandline();
-        showAllWords();
-    }
-
-    public  void   dictionaryAdvanced()
-    {
-        DictionaryManagement dm= new DictionaryManagement();
-        dm.insertFromFile();
-        dm.insertFromCommandline();
-        dm.dictionaryLookup();
-        showAllWords();
-    }
-    public  void showAllWords()
-    {
-        th=1;
-        System.out.println(String.format("%s %20s %20s %20s %20s ","No","|","English","|","Vietnamese"));
-        for (Word w: Dictionary.arrWord)
-        {
-            //System.out.println(th+"     | "+w.getWord_target()+"     | "+ w.getWord_explain()+" ");
-
-            System.out.println(String.format("%02d %20s %20s %20s %20s ", th, "|", w.getWord_target(), "|" ,w.getWord_explain()));
-            th++;
-
-        }
+    public static void main(String[] args) {
+        DictionaryCommandline dc= new DictionaryCommandline();
+        dc.dictionaryBasic();
+        dc.dictionaryAdvanced();
     }
 }
